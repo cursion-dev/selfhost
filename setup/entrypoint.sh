d62762016aea4c2e8eac5b7f3b76d6daf6210de5
Landon <l.rodden52@gmail.com>
2024-12-12 20:36:13 -0700

    testing

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -39,29 +39,29 @@ REPOSITORY="https://github.com/cursion-dev/selfhost.git"
 # fi
 
 # install dependencies
-apt-get install -y ca-certificates curl python3 docker.io
+apt-get install -y ca-certificates curl python3
 
 # check and install docker
-# docker --version || { 
-#     # uninstall old pkgs
-#     for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg -y; done
+docker --version || { 
+    # uninstall old pkgs
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg -y; done
     
-#     # download new
-#     apt-get install ca-certificates
-#     install -m 0755 -d /etc/apt/keyrings
-#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-#     chmod a+r /etc/apt/keyrings/docker.asc
+    # download new
+    apt-get install ca-certificates
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
 
-#     # Add the repository to Apt sources:
-#     echo \
-#         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#         $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#         tee /etc/apt/sources.list.d/docker.list > /dev/null
-#     apt-get update
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
     
-#     # install new docker
-#     apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-# }
+    # install new docker
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+}
 
 
 
