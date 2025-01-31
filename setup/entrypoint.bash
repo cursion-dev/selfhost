4b23223913b0648a96c4c3a134b9bf4b907fc6c2
Landon <l.rodden52@gmail.com>
2025-01-31 15:30:19 -0700

    test: testing script configuration

--- setup/entrypoint.bash
+++ setup/entrypoint.bash
@@ -135,13 +135,12 @@ echo 'Starting installer script'
 
 # Run installer.py setup script explicitly with Python
 python3 ./setup/installer/installer.py \ 
-    --license-key=$2 \ 
-    --admin-email=$3 \ 
-    --admin-pass=$4 \ 
-    --server-domain=$5 \ 
-    --client-domain=$6 \ 
-    --gpt-key=$7 \ 
-    </dev/tty
+    --license-key="$2" \ 
+    --admin-email="$3" \ 
+    --admin-pass="$4" \ 
+    --server-domain="$5" \ 
+    --client-domain="$6" \ 
+    --gpt-key="$7" </dev/tty
 
 
 
