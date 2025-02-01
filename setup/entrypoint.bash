fbf6a091cabb2128f93c95cc05286bf2a97014ff
Landon <l.rodden52@gmail.com>
2025-02-01 12:22:27 -0700

    test: new args handling

--- setup/entrypoint.bash
+++ setup/entrypoint.bash
@@ -134,13 +134,7 @@ pip3 install --user --break-system-packages -r ./setup/installer/requirements.tx
 echo 'Starting installer script'
 
 # Run installer.py setup script explicitly with Python
-python3 ./setup/installer/installer.py \ 
-    --license-key=$2 \ 
-    --admin-email=$3 \ 
-    --admin-pass=$4 \ 
-    --server-domain=$5 \ 
-    --client-domain=$6 \ 
-    --gpt-key=$7 -- </dev/tty
+python3 ./setup/installer/installer.py --license-key="$2" --admin-email="$3" --admin-pass="$4" --server-domain="$5" --client-domain="$6" --gpt-key="$7" -- </dev/tty
 
 
 
